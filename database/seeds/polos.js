f395da7ccd791ee58090c36d938d80c1b0a951aa
Jamison Blackwell <blackwellj1040@gmail.com>
2019-11-21 12:46:53 -0500

    add img to seed

--- database/seeds/polos.js
+++ database/seeds/polos.js
@@ -4,16 +4,16 @@ exports.seed = function(knex) {
  
       // Inserts seed entries
       return knex('polos').insert([
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24,  },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 },
-        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24 }
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+        {item_name: 'TommyHillfiger' , description: 'Made of all cotton. Excellent fit.', price: 24, img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }
 
 
       ]);
--- shop/src/components/CategoryOne.js
+++ shop/src/components/CategoryOne.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './CategoryOne.css';
 import NavBar from './NavBar';
-import Axios from 'axios';
+import axios from 'axios';
 
 const CategoryOne = () => {
     
-    const getPolos = function () {
+    useEffect(() => {
+        axios.get("https://shirt-store123.herokuapp.com/api/inventory")
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
 
-    }
+
 
     return (
         <div>
