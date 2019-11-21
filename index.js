cd6595c0bf87742dfac7df4e74d1d953c3b87478
Jamison Blackwell <blackwellj1040@gmail.com>
2019-11-21 11:16:56 -0500

    added line to index

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
+server.use(express.static('client'))
+
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-if (process.env.NODE_ENV === 'production') {
-	server.use(express.static('client/build'));
-}
-
-server.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
