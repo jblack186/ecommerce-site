212eef52999e1e90af141cacc15a8f2d5ce86cef
Jamison Blackwell <blackwellj1040@gmail.com>
2019-11-22 17:44:06 -0500

    got rid of pool

--- knexfile.js
+++ knexfile.js
@@ -40,11 +40,6 @@ module.exports = {
     },
   },
    // add the following
-   pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-    },
-  },
+ 
 
 };
