3c16d899530b5def402280ab986ffeae0ed9fa62
Jamison Blackwell <blackwellj1040@gmail.com>
2019-11-24 13:34:11 -0500

    ran seed file

--- inventory/inventory-router.js
+++ inventory/inventory-router.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 const Inventory = require('./inventory-model.js');
 
-// router.get('/', (req, res) => {
-//     Inventory.find()
-//     .then(polos => {
-//       res.status(200).json(polos);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.send(err)
-//     })
-//   })
+router.get('/', (req, res) => {
+    Inventory.find()
+    .then(polos => {
+      res.status(200).json(polos);
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err)
+    })
+  })
 
   module.exports = router;
