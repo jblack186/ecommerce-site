3b5726f551a456b9b819f90609f625102155500a
Jamison Blackwell <blackwellj1040@gmail.com>
2019-11-26 13:49:30 -0500

    commented out users

--- database/migrations/20191118075337_inventory.js
+++ database/migrations/20191118075337_inventory.js
@@ -13,14 +13,14 @@ exports.up = function(knex) {
 
   })
 
-  .createTable('users', tbl => {
-    tbl.increments();
-    tbl.string('username', 256)
-      .notNullable()
-    tbl.string('password', 256)
-      .notNullable()
-  })
-};
+//   .createTable('users', tbl => {
+//     tbl.increments();
+//     tbl.string('username', 256)
+//       .notNullable()
+//     tbl.string('password', 256)
+//       .notNullable()
+//   })
+// };
 
 exports.down = function(knex) {
   return knex.schema
