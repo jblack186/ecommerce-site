0074e90b18d0a84b77989f89b1135f0f7e46f00c
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-09 13:41:55 -0500

    push h

--- database/migrations/20191118075337_inventory.js
+++ database/migrations/20191118075337_inventory.js
@@ -1,10 +1,17 @@
 
 exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', tbl => {
+      tbl.increments()
+      tbl.string('username', 125).notNullable()
+      tbl.string('password', 125).notNullable()
+  })
 
 };
 
 exports.down = function(knex) {
-    
+  return knex.schema
+  .dropTableIfExists('users')
 
 };
 
