01c8e14999259273fa3d013bc5bf5e23286c2635
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-11 10:48:35 -0500

    commented out cart seed

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,11 +1,11 @@
 exports.seed = function(knex) {
         
   // Inserts seed entries
-  return knex('users').insert([
-    {username: 'jblack186', password: 'me'},
-    {username: 'jblack186', password: 'me'},
-    {username: 'jblack186', password: 'me'},
-    {username: 'jblack186', password: 'me'},
+  // return knex('users').insert([
+  //   {username: 'jblack186', password: 'me'},
+  //   {username: 'jblack186', password: 'me'},
+  //   {username: 'jblack186', password: 'me'},
+  //   {username: 'jblack186', password: 'me'},
 
-  ]);
+  // ]);
 };
