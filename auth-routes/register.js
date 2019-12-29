37cedde9fee000086e80d71f6610ef8b2a4ada08
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-29 11:22:46 -0500

    test

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -6,10 +6,11 @@ const cors = require('cors');
 
 //test
 
-const corsOptions = {
-origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.post('/', (req, res) => {
   let  user = req.body
