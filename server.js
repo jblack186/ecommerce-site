faa632a202c5ee72c9a6e8831cfcd1fa230f859d
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-29 13:59:43 -0500

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const dbEnv = process.env.DB_ENV || 'development';
 
 //CORS middleware
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
