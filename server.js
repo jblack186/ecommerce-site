0cf9b203fcf1777f8897cfc1bbb9114d1e9158f7
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 18:18:59 -0500

    test

--- server.js
+++ server.js
@@ -24,7 +24,13 @@ const dbEnv = process.env.DB_ENV || 'development';
 
 
 
+server.use(cors({
+  origin: "http://localhost:3000",
+    credentials: true,
+    withCredentials: true,
 
+
+  }));
 
 
 
