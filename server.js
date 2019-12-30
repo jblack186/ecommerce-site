5292124c2efe52badd702a91d25db81108f5d1a1
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 08:48:59 -0500

    test

--- server.js
+++ server.js
@@ -40,11 +40,18 @@ server.use(cors({
   }));
 
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  server.use((req , res , next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header("Access-Control-Allow-Credentials", true);
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Content-Types, Authorization, Application/JSON');
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST,PATCH,DELETE, GET');
+    return res.status(200).json({});
+  }
   next();
 });
+
 
 
 // server.use(function(req, res, next) {
