54a6b5cd081e6c0bc1c98b8f26249e83665c8517
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 13:46:19 -0500

    test

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ const sessionConfig = {
     cookie: {
         maxAge: 1000 * 600 * 10,
         store: store,
-        sameSite: 'none',
+        sameSite: null,
         secure: process.env.NODE_ENV == 'development' ? false : true, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
         httpOnly: true // if true JS cannot access the cookie
     },
