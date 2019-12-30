5cc21b6a114bc3a0ccfc62c42271bd73a77c98cb
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 18:33:03 -0500

    test

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const sessionConfig = {
     resave: false, // save sessions even when they are not changed
     cookie: {
         maxAge: 1000 * 600 * 10,
-        sameSite: 'lax',
+        sameSite: 'strict',
         secure: false, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
         httpOnly: true // if true JS cannot access the cookie
     },
