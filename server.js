1ea7553c7444bcdf67332c2d5e1646580a3ca5ab
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 01:04:56 -0500

    test

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+  req.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  req.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 }
 
