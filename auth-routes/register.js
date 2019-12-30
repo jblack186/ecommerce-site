50f022ffc3086e0c001b7646e555a367d5612e30
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-29 20:13:04 -0500

    test

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 //test
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Access-Control-Allow-Origin", "*");    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
