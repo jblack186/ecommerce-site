766a77af4e594e6b372a7a78e75579f5f8679c0e
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-30 09:05:36 -0500

    test

--- server.js
+++ server.js
@@ -40,17 +40,17 @@ server.use(cors({
   }));
 
 
-  server.use((req , res , next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  //   res.header("Access-Control-Allow-Credentials", true);
-  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Content-Types, Authorization, Application/JSON');
-  if(req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-Methods', 'PUT, POST,PATCH,DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+//   server.use((req , res , next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   //   res.header("Access-Control-Allow-Credentials", true);
+//   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Content-Types, Authorization, Application/JSON');
+//   if(req.method === 'OPTIONS'){
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST,PATCH,DELETE, GET');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 
 
