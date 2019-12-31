3a92de5caf083de377656357794be499a0e89826
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-31 16:45:54 -0500

    test

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,17 @@
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/inventory.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
-    },
+    // development: {
+    //   client: 'sqlite3',
+    //   connection: {
+    //     filename: './database/inventory.db3',
+    //   },
+    //   useNullAsDefault: true,
+    //   migrations: {
+    //     directory: './database/migrations',
+    //   },
+    //   seeds: {
+    //     directory: './database/seeds',
+    //   },
+    // },
     testing: {
       client: 'sqlite3',
       connection: {
