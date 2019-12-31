052c5939070ea49514fca55c921cc149d32c0f71
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-31 15:28:46 -0500

    test

--- shop/src/components/Register.js
+++ shop/src/components/Register.js
@@ -20,7 +20,7 @@ export default class Register extends React.Component {
 
     register = (e) => {
         e.preventDefault();
-        axios.post('https://shirt-store123.herokuapp.com/api/register', this.state, {headers :  {'Access-Control-Allow-Origin': 'http://localhost:3000/'} })
+        axios.post('https://shirt-store123.herokuapp.com/api/register', this.state,)
         .then(response => {
             console.log('cool, cool')
         //    this.props.history.push('/login') 
