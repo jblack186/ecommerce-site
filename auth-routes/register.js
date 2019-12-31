c98d2ef11fa430ba51067e2a0837e0fcb358ac64
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-31 13:05:41 -0500

    test

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 // });
 
 const auth = router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   next();
 });
 
