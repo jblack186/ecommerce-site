52b8b91603d6aac9d570e7198d9ef792992d99e4
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-31 15:41:20 -0500

    test

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -32,7 +32,7 @@ router.post('/',auth, (req, res) => {
     Users.addUser(user)
    
       .then(user => {
-        req.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        user.req.header("Access-Control-Allow-Origin", "http://localhost:3000");
 
         // req.session.user = user
         // const token = generateToken(newUser)
