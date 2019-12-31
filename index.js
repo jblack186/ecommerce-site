1ad005c0bfd64e465544006c502d894bf7188630
Jamison Blackwell <blackwellj1040@gmail.com>
2019-12-31 15:35:22 -0500

    test

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
+ server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  });
+
 // server.use(express.static('client'))
 
 const PORT = process.env.PORT || 8000;
