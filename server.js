a6087d3f7c0d6633fbceec6ef786079a03dad55d
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-01 09:55:00 -0500

    test

--- server.js
+++ server.js
@@ -90,10 +90,10 @@ const sessionConfig = {
     cookie: {
         maxAge: 1000 * 600 * 10,
         secure: false, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
-        httpOnly: true // if true JS cannot access the cookie
+        httpOnly: false // if true JS cannot access the cookie
     },
     store: new KnexSessionStore({ // DONT FORGET new KEYWORD //how to store sessions
-      knex: knex,
+      // knex: knex,
       tablename: "session"
   
 }),
