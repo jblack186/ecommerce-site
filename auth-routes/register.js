22d5e8dd250b166aeb953d250d5d8be676c782e8
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-01 12:51:28 -0500

    test

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -10,11 +10,11 @@ const cors = require('cors');
 //     withCredentials: true,
 // });
 
-// router.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // router.use(cors({
 //   origin: "http://shirt-store123.herokuapp.com",
