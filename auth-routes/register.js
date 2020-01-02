4327e97825ffca18b16d5635acd5b92301c43c4f
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-02 00:09:12 -0500

    tes

--- auth-routes/register.js
+++ auth-routes/register.js
@@ -28,6 +28,9 @@ router.use(function(req, res, next) {
 
 router.post('/', (req, res) => {
   let  user = req.body
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   const hash = bcrypt.hashSync(user.password, 10)
   user.password=hash
   if(!user.username || !user.password ) {
@@ -35,8 +38,9 @@ router.post('/', (req, res) => {
   } else {
     Users.addUser(user)
       .then(user => {
-        req.header("Access-Control-Allow-Origin", "http://localhost:3000");
-        res.status(201).json({user})
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+              res.status(201).json({user})
 
         // const token = generateToken(newUser)
 
