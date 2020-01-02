93aaf10677419caa0061c7c6df790f98aab07a8e
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-01 22:26:29 -0500

    t

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const knex = Knex({
   connection: {
     host: '127.0.0.1',
     port: '5432',
-    database: 'jamisonblackwell',
+    database: 'postgres',
     user: "postgres",
     password: ""
     
