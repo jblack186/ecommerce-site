7a18b67990eeeb16f33223a1fe87364481598305
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-01 23:59:38 -0500

    test

--- auth-routes/login.js
+++ auth-routes/login.js
@@ -14,6 +14,12 @@ router.use(cors({
 
   }));
 
+  router.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  });
+
 router.post('/', (req, res) => {
   let {username, password} = req.body
 
