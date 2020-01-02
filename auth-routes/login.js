6e4e630ccc6c5d71e98c36e62aa6f6a5600e8b67
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-02 15:28:31 -0500

    t

--- auth-routes/login.js
+++ auth-routes/login.js
@@ -14,11 +14,12 @@ const cors = require('cors');
 
 //   }));
 
-  // router.use(function(req, res, next) {
-  //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  //   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //     next();
-  // });
+router.use(function(req, res, next) {
+  req.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 router.post('/', (req, res) => {
   let {username, password} = req.body
