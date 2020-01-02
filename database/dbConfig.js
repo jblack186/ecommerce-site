5e99365f6bf7acfbb4df393e8361765f4874e345
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-02 14:53:44 -0500

    t

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,8 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-// const dbEnv = process.env.DB_ENV || 'production';
+const dbEnv = process.env.DB_ENV || 'production';
 
-const dbEnv = process.env.DB_ENV || 'development';
+// const dbEnv = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[dbEnv]);     
