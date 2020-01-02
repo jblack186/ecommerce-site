7dfb2447aa1fbab38ff9b4149263a5385dfffb39
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-02 09:27:39 -0500

    t

--- server.js
+++ server.js
@@ -28,14 +28,7 @@ const dbEnv = process.env.DB_ENV || 'production';
 const Knex = require("knex");
 const knex = Knex({
   client: "pg",
-  connection: {
-    host: '127.0.0.1',
-    port: '5432',
-    database: 'jamisonblackwell',
-    user: "postgres",
-    password: ""
-    
-  },
+  connection: process.env.DATABASE_URL,
  
 })
 
