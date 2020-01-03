fcaa264e463a400468c439d0f7d54d38041377d1
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-03 00:13:17 -0500

    t

--- knexfile.js
+++ knexfile.js
@@ -30,8 +30,15 @@ module.exports = {
   
     production: {
       client: 'pg',
-      connection: process.env.DATABASE_URL,
-       useNullAsDefault: true,
+      connection: {
+        host: 'ec2-54-243-241-62.compute-1.amazonaws.com',
+        database: 'd2vhhe21pl74f3',
+        user: "izkbkalkiehxvz",
+        password: "bc14ad9345b95898a896d289fc662044efe2db40d37cb9d054a7a84f31a15a60",
+        port: '5432',
+        ssl: true
+        
+      },
            migrations: {
         directory: './database/migrations',
       },
