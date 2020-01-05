5ff86be7abc2d4f366f45fb10c2bd28aa5d08982
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-04 19:43:07 -0500

    t

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ server.use(session({
   name: 'cocoabutter',
   secret: 'cocoabutter',
   saveUninitialized: true, // has implications with GDPR laws
-  resave: false, // save sessions even when they are not changed
+  resave: true, // save sessions even when they are not changed
   cookie: {
       maxAge: 10000 * 600 * 60 * 24,
       secure: false, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
