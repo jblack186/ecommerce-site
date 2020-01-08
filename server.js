a109608eebfee943f2fe026d7f88577251e8899e
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-08 11:12:44 -0500

    t

--- server.js
+++ server.js
@@ -68,7 +68,8 @@ server.use(session({
       maxAge: 10000 * 600 * 60 * 24,
       secure: false, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
       httpOnly: true, // if true JS cannot access the cookie
-      sameSite: true
+      sameSite: true,
+      credentials: "include"
       // rolling: true
   },
   store: store
