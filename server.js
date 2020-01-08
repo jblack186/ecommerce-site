d6f77d180d199f42370bf6b3a66a2bf42ad320f5
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-08 13:47:26 -0500

    t

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ server.use(session({
   resave: false, // save sessions even when they are not changed
   cookie: {
       maxAge: 10000 * 600 * 60 * 24,
-      secure: false, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
+      secure: true, //in production set this to true cuz should only be sent if https // if false the cookie is sent over http, if true only sent over https
       httpOnly: true, // if true JS cannot access the cookie
       sameSite: true,
       credentials: true
