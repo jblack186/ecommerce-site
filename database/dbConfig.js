05cf7122d58488ab0d8cff4f694940304910db6a
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-09 12:41:16 -0500

    t

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 const config = require('../knexfile.js');
 
-const environment = 'production'; // if something else isn't setting ENV, use development
+const environment = 'development'; // if something else isn't setting ENV, use development
 
 module.exports = knex(config[environment]);
 
