2c42e281b27dcf91403941c135e45d9e72594eb6
Jamison Blackwell <blackwellj1040@gmail.com>
2020-01-09 12:27:45 -0500

    t

--- knexfile.js
+++ knexfile.js
@@ -8,36 +8,48 @@ const localPg = {
 }
 
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/inventory.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
-    },
-    testing: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
-    },
+    // development: {
+    //   client: 'sqlite3',
+    //   connection: {
+    //     filename: './database/inventory.db3',
+    //   },
+    //   useNullAsDefault: true,
+    //   migrations: {
+    //     directory: './database/migrations',
+    //   },
+    //   seeds: {
+    //     directory: './database/seeds',
+    //   },
+    // },
+    // testing: {
+    //   client: 'sqlite3',
+    //   connection: {
+    //     filename: './database/test.db3',
+    //   },
+    //   useNullAsDefault: true,
+    //   migrations: {
+    //     directory: './database/migrations',
+    //   },
+    //   seeds: {
+    //     directory: './database/seeds',
+    //   },
+    // },
      
 
   
     production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      useNullAsDefault: true,
+           migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      },
+    },
+
+    development: {
       client: 'pg',
       connection: localPg,  
       useNullAsDefault: true,
@@ -50,3 +62,10 @@ module.exports = {
     },
 
 };
+
+
+
+
+
+
+  
